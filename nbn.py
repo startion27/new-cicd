8601c8b25233d4e8f72d8e9832a93050f6d34ff0
startion27 <startion27@gmail.com>
2025-08-26 12:03:13 +0900

    Create nbn.py

---
+++ nbn.py
@@ -0,0 +1,8 @@
+def gugudan():
+    for i in range(2, 10):
+        for j in range(1, 10):
+            print(f"{i} x {j} = {i * j}")
+        print("-" * 15)
+
+if __name__ == "__main__":
+    gugudan()
